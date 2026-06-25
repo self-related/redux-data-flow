deccf8ee16608433582165bb597d4fa69e99d1f4
Nekit <nick.misiura@yandex.ru>
2026-06-25 07:46:01 +0300

    Add SEO for /posts/:index

--- src/pages/posts-page/PostsPage.tsx
+++ src/pages/posts-page/PostsPage.tsx
@@ -2,11 +2,22 @@ import { useParams } from "react-router";
 import { Post } from "../../features/posts/ui/post/Post";
 import styles from "./PostsPage.module.css";
 import { useSelectPostByIndex } from "../../features/posts/model/postsSlice";
-
+import { useEffect } from "react";
+import { setMeta } from "../../shared/lib/seo";
+import { DEFAULT_META } from "../../shared/metadata/defaults";
 
 export default function PostPage() {
-   const postIndex = Number(useParams().postIndex); 
-   const post = useSelectPostByIndex(postIndex);
+    const postIndex = Number(useParams().postIndex); 
+    const post = useSelectPostByIndex(postIndex);
+
+    // meta
+    const pageTitle = post?.title ?? "Not found";
+    const pageDescription = post?.content.slice(0, 255) ?? "";
+    
+    useEffect(() => setMeta({ 
+        title: `${pageTitle} - ${DEFAULT_META.title}`, 
+        description: pageDescription 
+    }), [pageTitle, pageDescription]);
    
    if (!post) {
     return (
