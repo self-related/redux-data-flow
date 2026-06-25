3adf6e43101bcc7f5ddbed82be8f40a5fac8a063
Nekit <nick.misiura@yandex.ru>
2026-06-25 09:55:16 +0300

    Add title for login page

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router";
 import { useSelectAllUsers } from "../../features/users/model/usersSlice";
 import styles from "./LoginPage.module.css";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Button from "../../shared/ui/Button";
 import { userLoggedIn } from "../../features/auth/model/authSlice";
 import { useAppDispatch } from "../../app/model/store";
+import { setMeta } from "../../shared/lib/seo";
 
+
+const pageTitle = "Login";
 
 export default function LoginPage() {
     const navigate = useNavigate();
@@ -23,18 +26,22 @@ export default function LoginPage() {
         navigate("/");
     };
 
+    useEffect(() => {
+        setMeta({
+            title: pageTitle
+        });
+    }, []);
 
+    
     return (
         <div className={styles.loginPage}>
             <select ref={userSelectElement} className={styles.userSelect}>
                 <option value="" hidden>choose user</option>
-
                 {
                     users?.map(({ id, name }) => (
                         <option key={id} value={id}>{name}</option>
                     ))
                 }
-
             </select>
 
             <Button onClick={handleLogin}>
